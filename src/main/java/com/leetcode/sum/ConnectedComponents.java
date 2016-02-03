ce13711ce4e84114bfbcb9e06cd9b4592c1ce8d7
Steve <wsapiens@hotmail.com>
2016-02-02 23:01:03 -0600

    accepted solution for number of connected components

--- src/main/java/com/leetcode/sum/ConnectedComponents.java
+++ src/main/java/com/leetcode/sum/ConnectedComponents.java
@@ -5,6 +5,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Number of Connected Components in an Undirected Graph
+ * https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+ * 
+ * @author spark
+ *
+ */
 public class ConnectedComponents {
 	public int countComponents(int n, int[][] edges) {
 		List<Set<Integer>> components= new ArrayList<>();
@@ -15,17 +22,9 @@ public class ConnectedComponents {
 				for(int i = 0; i < components.size(); i++) {
 					if(components.get(i).contains(edge[0])) {
 						compNum1 = i;
-						if(!components.get(i).contains(edge[1])) {
-							components.get(i).add(edge[1]);
-							compNum2 = i;
-						}
 					}
 					if(components.get(i).contains(edge[1])) {
 						compNum2 = i;
-						if(!components.get(i).contains(edge[0])) {
-							components.get(i).add(edge[0]);
-							compNum1 = i;
-						}
 					}
 				}
 				if( compNum1 == -1 && compNum2 == -1) {
@@ -33,103 +32,109 @@ public class ConnectedComponents {
 					set.add(edge[0]);
 					set.add(edge[1]);
 					components.add(set);
-				} else if( compNum1 != compNum2) {
+				} else if( compNum1 != -1 && compNum2 == -1) {
+					components.get(compNum1).add(edge[1]);
+					compNum2 = compNum1;
+				} else if ( compNum1 == -1 && compNum2 != -1) {
+					components.get(compNum2).add(edge[0]);
+					compNum1 = compNum2;
+				} else if( compNum1 != -1 && compNum2 != -1 && compNum1 != compNum2) {
 					components.get(compNum1).addAll(components.get(compNum2));
 					components.remove(compNum2);
 				}
 			}
 		}
-		
-		return components.size() + (n - components.stream().map(comp -> comp.size()).reduce(0, (a, b) -> a+b));
+		return components.size() + (n - components.stream().mapToInt(comp -> comp.size()).sum());
 	}
 
 	public static void main(String[] args) {
-		//		5
-		//		[[0,1],[1,2],[0,2],[3,4]]
+//		5
+//		[[0,1],[1,2],[0,2],[3,4]]     => 2
 		int [][] edges = {{0,1}, {1,2}, {0,2}, {3,4}};
 		System.out.println( new ConnectedComponents().countComponents(5, edges) );
 
-		//		n = 5 and edges = [[0, 1], [1, 2], [3, 4]]
+//		n = 5 and edges = [[0, 1], [1, 2], [3, 4]]	=> 2
 		int [][] edges1 = {{0,1}, {1,2}, {3,4}};
 		System.out.println( new ConnectedComponents().countComponents(5, edges1) );
 
-		//		n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]]
+//		n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]] 		=> 1
 		int [][] edges2 = {{0,1}, {1,2}, {2, 3}, {3,4}};
 		System.out.println( new ConnectedComponents().countComponents(5, edges2) );
 
+//		0
+//
+//		1
+//		| \
+//		2 - 3
 
-		//		0
-		//
-		//		1
-		//		| \
-		//		2 - 3
-
-		//		4
-		//		[[2,3],[1,2],[1,3]]
+//		4
+//		[[2,3],[1,2],[1,3]]		=> 2
 		int [][] edges3 = {{2, 3}, {1,2}, {1,3}};
 		System.out.println( new ConnectedComponents().countComponents(4, edges3) );
 
+//		4
+//		[[0,1],[2,3],[1,2]]		=> 1
 
-		//		4
-		//		[[0,1],[2,3],[1,2]]
-
-		//		0
-		//		|
-		//		1 - 2 - 3
+//		0
+//		|
+//		1 - 2 - 3
 		int [][] edges4 = {{0,1}, {2,3}, {1,2}};
 		System.out.println( new ConnectedComponents().countComponents(4, edges4) );
 
 		int [][] edges5 = {};
 		System.out.println( new ConnectedComponents().countComponents(0, edges5) );
 
-		//		6
-		//		[[0,1],[0,2],[2,5],[3,4],[3,5]]
-		//	            3 - 4
-		//		        |
-		//		0 - 2 - 5
-		//		|
-		//		1
-
+//		6
+//		[[0,1],[0,2],[2,5],[3,4],[3,5]]		=> 1
+//	            3 - 4
+//		        |
+//		0 - 2 - 5
+//		|
+//		1
 		int [][] edges6 = {{0,1},{0,2},{2,5},{3,4},{3,5}};
 		System.out.println( new ConnectedComponents().countComponents(6, edges6) );
 
-		//		5
-		//		[[2,3],[1,2],[1,3],[0,4]]
+//		5
+//		[[2,3],[1,2],[1,3],[0,4]]	=> 2
 
-		//		2 - 1
-		//		|  /
-		//		3
-		//		
-		//		0 - 4
+//		2 - 1
+//		|  /
+//		3
+//		
+//		0 - 4
 
+//		5
+//		| \
+//		2 - 3 - 4    => 1
+//		|
+//		0
 		int [][] edges7 = {{2,3},{0,2},{2,5},{3,4},{3,5}};
 		System.out.println( new ConnectedComponents().countComponents(5, edges7) );
 
-		//		2
-		//		[[0,1]]
+//		2
+//		[[0,1]]
 		int [][] edges8 = {{0,1}};
 		System.out.println( new ConnectedComponents().countComponents(2, edges8) );
 
-		//		6
-		//		[[0,1],[1,2],[2,0],[3,4],[4,5]]
+//		6
+//		[[0,1],[1,2],[2,0],[3,4],[4,5]]
 
-		//		0 
-		//		| \
-		//		1 - 2  3 - 4 - 5
+//		0 
+//		| \
+//		1 - 2  3 - 4 - 5
 		int [][] edges9 = {{0,1}, {1,2}, {2,0},{3,4},{4,5}};
 		System.out.println( new ConnectedComponents().countComponents(6, edges9) );
 
-		//		8
-		//		[[1,0],[2,6],[1,7],[6,4],[7,0],[0,5]]
+//		8
+//		[[1,0],[2,6],[1,7],[6,4],[7,0],[0,5]]
 
-		//		1 - 0 - 5
-		//		| /
-		//		7
-		//		
-		//		2 - 6 - 4
+//		1 - 0 - 5
+//		| /
+//		7
+//		
+//		2 - 6 - 4
 		int [][] edges10 = {{1,0},{2,6},{1,7},{6,4},{7,0},{0,5}};
 		System.out.println( new ConnectedComponents().countComponents(8, edges10) );
-
 	}
 }
 
