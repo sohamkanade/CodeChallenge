08b647a5f4097564cd1b76f186cd86be2d8e7ed7
Steve <wsapiens@hotmail.com>
2016-03-08 08:09:32 -0600

    hashcode and equal methods

--- src/main/java/com/walmart/PrimeNumber.java
+++ src/main/java/com/walmart/PrimeNumber.java
@@ -14,4 +14,22 @@ public class PrimeNumber {
 		}
 		return true;
 	}
+
+	public boolean isPrimeNumber2(int input) {
+		if(input <= 0 ) {
+			throw new IllegalArgumentException(String.join(": ", "Got inpvalid input", String.valueOf(input)));
+		}
+
+		for(int i = 2; i <= (int)Math.sqrt(input); i++) {
+			if( input % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		PrimeNumber pn = new PrimeNumber();
+		System.out.println( pn.isPrimeNumber2(11) );
+	}
 }
